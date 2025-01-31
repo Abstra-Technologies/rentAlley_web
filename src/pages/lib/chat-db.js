cf7c874787e254982ed60c663f3b3527050cf458
BryLim-AI <katoshi.bryan@gmail.com>
2025-01-31 13:38:04 +0800

    ADD: Chat DB

---
+++ src/pages/lib/chat-db.js
@@ -0,0 +1,13 @@
+const mysql = require("mysql2/promise");
+
+const pool = mysql.createPool({
+    host: 'rentahan-db.ctmeauk6wkzd.ap-southeast-1.rds.amazonaws.com',
+    user: 'rentahan_dev', // Default value for debugging
+    password: 'Rentahan_db_admin2024',
+    database: 'rentahan',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+module.exports = pool;
