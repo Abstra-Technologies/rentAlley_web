f793f289097a2cbe737dbbfce0d3ff032dca67bb
BryLim-AI <katoshi.bryan@gmail.com>
2025-01-31 14:50:04 +0800

    initial bug report temporary ui deign only

--- src/app/pages/tenant/dashboard/page.js
+++ src/app/pages/tenant/dashboard/page.js
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import useAuth from "../../../../../hooks/useSession";
 import ChatComponent from "../../../../components/modules/chat";
+import {useRouter} from "next/navigation";
 
 /**
  * TODO
@@ -13,7 +14,7 @@ import ChatComponent from "../../../../components/modules/chat";
 
 export default function TenantDashboard() {
   const { user, loading, error, signOut } = useAuth();
-
+const router = useRouter();
   if (loading) {
     return <p>Loading...</p>;
   }
@@ -25,28 +26,36 @@ export default function TenantDashboard() {
   console.log("User Data ", user);
 
   return (
-    <div>
-      <h1>
-        Welcome, {user.firstName} {user.lastName}!
-      </h1>
-      <p>Your user type is: {user.userType}  | ID: {user.userID}</p>
+      <div>
+          <h1>
+              Welcome, {user.firstName} {user.lastName}!
+          </h1>
+          <p>Your user type is: {user.userType} | ID: {user.userID}</p>
 
-      {/* View Profile Button */}
-      <Link href={`/pages/${user.userType}/profile/${user.userID}`}>
-        <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
-          View Profile
-        </button>
-      </Link>
-        <ChatComponent user={user} />
-      {/* Sign Out Button */}
-      <button
-        onClick={signOut}
-        className="bg-red-500 text-white px-4 py-2 rounded-md ml-4"
-      >
-        Sign Out
-      </button>
+          {/* View Profile Button */}
+          <Link href={`/pages/${user.userType}/profile/${user.userID}`}>
+              <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
+                  View Profile
+              </button>
+          </Link>
+          <ChatComponent user={user}/>
 
-      {/* Additional content can go here */}
-    </div>
+          <button
+              onClick={() => router.push("/pages/commons/bug-report")}
+              className="bg-yellow-500 text-white px-4 py-2 rounded-md hover:bg-yellow-600 transition"
+          >
+              Report a Bug
+          </button>
+
+          {/* Sign Out Button */}
+          <button
+              onClick={signOut}
+              className="bg-red-500 text-white px-4 py-2 rounded-md ml-4"
+          >
+              Sign Out
+          </button>
+
+          {/* Additional content can go here */}
+      </div>
   );
 }
