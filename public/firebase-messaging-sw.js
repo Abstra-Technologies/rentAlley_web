3096f3808e6798b134bf7547060b3cbd6b83e93e
BryLim-AI <katoshi.bryan@gmail.com>
2025-02-25 19:39:23 +0800

    modify firebase fcm

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,4 +1,3 @@
-import firebase from "firebase/compat";
 
 importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
