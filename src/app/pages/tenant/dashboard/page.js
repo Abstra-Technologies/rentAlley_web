4f9bc3ed7ededb86205b5ea968fc489e118b4aae
BryLim-AI <katoshi.bryan@gmail.com>
2025-03-01 15:03:31 +0800

    modified tenant dashboard included annoucement widget

--- src/app/pages/tenant/dashboard/page.js
+++ src/app/pages/tenant/dashboard/page.js
@@ -1,14 +1,13 @@
 "use client";
-import useAuth from "../../../../../hooks/useSession";
 import {useRouter} from "next/navigation";
 import TenantLayout from "../../../../components/navigation/sidebar-tenant";
 import useAuthStore from "../../../../zustand/authStore";
 import {useEffect, useState} from "react";
 import LoadingScreen from "../../../../components/loadingScreen";
+import Announcements from "../../../../components/annoucemen/announcement";
 
 export default function TenantDashboard() {
-  // const { user,loading, error, signOut } = useAuth();
-  // const { user } = useAuthStore();
+
     const { user, admin, fetchSession, loading } = useAuthStore();
     const [dataLoading, setDataLoading] = useState(true);
     const router = useRouter();
@@ -41,23 +40,18 @@ export default function TenantDashboard() {
   if (!user) {
     return <p>You need to log in to access the dashboard.</p>;
   }
-  console.log("User Data ", user);
   return (
       <TenantLayout>
       <div>
           <h1>
               Welcome, {user.firstName} {user.lastName}!
           </h1>
-          <p>Your user type is: {user.userType} | ID: {user?.user_id}</p>
-          <p>Tenant ID: {user.tenant_id}</p>
 
-          {/*<ChatComponent user={user}/>*/}
-          {/*<button*/}
-          {/*    onClick={() => router.push("/pages/commons/bug-report")}*/}
-          {/*    className="bg-yellow-500 text-white px-4 py-2 rounded-md hover:bg-yellow-600 transition"*/}
-          {/*>*/}
-          {/*    Report a Bug*/}
-          {/*</button>*/}
+          <div style={{ width: 500, height: 500 }}>
+              <Announcements userType={user.userType} />
+          </div>
+
+
 
       </div>
       </TenantLayout>
