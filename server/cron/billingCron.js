ea36afc40b5dd1115ad3f14b7bc8c370f7bbb914
BryLim-AI <katoshi.bryan@gmail.com>
2025-03-12 09:17:07 +0800

    Added CRON for Billing Notif 1 week

--- server/cron/billingCron.js
+++ server/cron/billingCron.js
@@ -5,13 +5,13 @@ console.log("Scheduled task to downgrade expired subscriptions.");
 
 // Run every midnight (12:00 AM)
 cron.schedule("0 13 * * *", async () => {
-    console.log(" Running scheduled check for expired subscriptions...");
+    console.log(" Running scheduled check for Billing Notification...");
 
     try {
-        const response = await axios.post("http://localhost:3000/api/subscription/downgrade");
+        const response = await axios.post("http://localhost:3000/utils/billingCron");
         console.log("Downgrade API Response:", response.data);
     } catch (error) {
-        console.error("Failed to downgrade expired subscriptions:");
+        console.error("Failed to send Billing");
         console.error("Status:", error.response?.status || "Unknown");
         console.error("Response:", error.response?.data || "No response");
         console.error("Error message:", error.message);
