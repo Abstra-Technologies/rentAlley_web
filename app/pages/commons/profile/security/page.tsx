6126ffa929f97d5666c30112a09d35577aa5b40b
Mattszuu <Mattuntalan2@gmail.com>
2025-11-15 02:09:51 +0800

    style(security-page): ui ux

--- app/pages/commons/profile/security/page.tsx
+++ app/pages/commons/profile/security/page.tsx
@@ -6,14 +6,13 @@ import axios from "axios";
 import SideNavProfile from "@/components/navigation/sidebar-profile";
 import SecurityPage from "@/components/Commons/settings/securityPrivacy";
 
-
 export default function LandlordSecurityPrivacy() {
-    return (
-        <div className="flex flex-col md:flex-row min-h-screen">
-            <SideNavProfile/>
-            <div className="flex-grow">
-                <SecurityPage/>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-col md:flex-row min-h-screen">
+      <SideNavProfile />
+      <div className="flex-grow md:ml-72">
+        <SecurityPage />
+      </div>
+    </div>
+  );
 }
