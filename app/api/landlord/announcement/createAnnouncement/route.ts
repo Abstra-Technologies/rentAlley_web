975b6175fcbfe411a62ee9fc6f71f49d16401843
BryLim-AI <katoshi.bryan@gmail.com>
2025-11-26 16:14:09 +0800

    fix(deployment): edge runtime

--- app/api/landlord/announcement/createAnnouncement/route.ts
+++ app/api/landlord/announcement/createAnnouncement/route.ts
@@ -1,3 +1,5 @@
+export const runtime = "nodejs";
+
 import { NextRequest, NextResponse } from "next/server";
 import { db } from "@/lib/db";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
