7f24b8f1440dc8e9f799668efd9a3cfd3c8a8b5d
Mattszuu <Mattuntalan2@gmail.com>
2025-11-30 22:41:45 +0800

    refactor(profile): remove loading state

--- app/pages/commons/profile/page.tsx
+++ app/pages/commons/profile/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import ProfilePage from "@/components/Commons/profilePage";
-import SideNavProfile from "@/components/navigation/sidebar-profile";
 
 export default function UserProfiles() {
   return (
