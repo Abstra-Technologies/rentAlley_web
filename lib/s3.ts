636f074dc4a652c9b32fb337c07bcb04c5516c1d
BryLim-AI <katoshi.bryan@gmail.com>
2025-12-18 15:17:07 +0800

    chore(aws): s3 updated to v3

---
+++ lib/s3.ts
@@ -0,0 +1,49 @@
+import {
+    S3Client,
+    PutObjectCommand,
+    DeleteObjectCommand,
+} from "@aws-sdk/client-s3";
+
+const s3 = new S3Client({
+    region: process.env.NEXT_AWS_REGION!,
+    credentials: {
+    accessKeyId: process.env.NEXT_AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.NEXT_AWS_SECRET_ACCESS_KEY!,
+},
+});
+
+export async function deleteFromS3(fileUrl: string) {
+    const bucket = process.env.NEXT_S3_BUCKET_NAME!;
+
+    const key = decodeURIComponent(
+        new URL(fileUrl).pathname.replace(/^\/+/, "")
+    );
+
+    await s3.send(
+        new DeleteObjectCommand({
+            Bucket: bucket,
+            Key: key,
+        })
+    );
+
+    console.log("✅ S3 deleted:", key);
+}
+
+export async function uploadToS3(
+    buffer: Buffer,
+    key: string,
+    contentType: string
+) {
+    const bucket = process.env.NEXT_S3_BUCKET_NAME!;
+
+    await s3.send(
+        new PutObjectCommand({
+            Bucket: bucket,
+            Key: key,
+            Body: buffer,
+            ContentType: contentType,
+        })
+    );
+
+    return `https://${bucket}.s3.${process.env.NEXT_AWS_REGION}.amazonaws.com/${key}`;
+}
