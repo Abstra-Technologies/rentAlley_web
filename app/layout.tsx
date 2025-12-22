31b683253e4360e27e9c79f47ec97b59cb9892c8
Kenshin_Bryz <84550697+BryLim-AI@users.noreply.github.com>
2025-12-22 06:03:17 +0000

    refactor(style): changes app icon to upkyp

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,7 +59,7 @@ export default function RootLayout({
         <ClientLayout>
 
             {children}
-            
+      
             <CookiesPermission />
             {/*<GoogleTranslateProvider />*/}
             {/* <FeedbackWidget /> */}
