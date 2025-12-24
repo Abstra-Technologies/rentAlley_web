5a1224500a1ca575535df6c593897f31ace5dc3d
Mattszuu <Mattuntalan2@gmail.com>
2025-12-24 15:12:11 +0800

    refactor(unitType): added studio

--- constant/unitTypes.ts
+++ constant/unitTypes.ts
@@ -1,5 +1,6 @@
 // constant/unitTypes.ts
 const unitTypes = [
+    { value: "studio", label: "Studio" },
     { value: "1-bedroom", label: "1 Bedroom" },
     { value: "2-bedroom", label: "2 Bedroom" },
     { value: "3-bedroom", label: "3 Bedroom" },
